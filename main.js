184bb066b88f5ec86158c626991add9198451056
sagaraivale <sagaraivale@hotmail.com>
2016-11-29 13:40:47 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+function getBox(x)
+{
+    var tbox={
+        length:10,
+        breadth:12,
+        height:5,
+        cost:x
+        };
+        return tbox;
+ }
+ 
+ var b1=getBox(100);
+ console.log(b1);
